2f559cd93aebf0df2a7272177cccbd139573bc8b
karlosmaor <karlosmaor94@gmail.com>
2018-02-10 15:04:40 -0500

    1.2.4

--- models/domiciliario.js
+++ models/domiciliario.js
@@ -38,7 +38,7 @@ DomiciliarioSchema.pre('save',function(next){
   })
 })
 
-DomiciliarioSchema.pre('update',function(next){
+DomiciliarioSchema.pre('findByIdAndUpdate',function(next){
   let dom = this
   if(dom.password == "") return next()
 
