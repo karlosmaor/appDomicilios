87549921a97884b947565511386b53ac57d0c0b5
karlosmaor <karlosmaor94@gmail.com>
2018-02-10 11:37:07 -0500

    1.0.6

--- server/main.js
+++ server/main.js
@@ -4,8 +4,6 @@ const mongoose =  require('mongoose')
 const app  = require('./app')
 const config = require('../config')
 
-console.log(Date.getTimezoneOffset())
-
 mongoose.connect(config.db, (err,res)=>{
   if(err){
     return console.log(`Error al conectarse a la base de datos: ${err}`);
