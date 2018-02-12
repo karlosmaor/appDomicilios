67b2ed59cbeb747340cb3fc8997872c36eff8465
karlosmaor <karlosmaor94@gmail.com>
2018-02-11 19:52:27 -0500

    1.24

--- controllers/domiciliario.js
+++ controllers/domiciliario.js
@@ -31,7 +31,7 @@ function updateDomiciliario(req,res){
 
   Domiciliario.findByIdAndUpdate(domiciliarioId, update, (err, domiciliarioUpdated) =>{
     if(err) return res.status(500).send({message:`Error al editar el domiciliario en la base de datos ${err}`})
-
+    console.log(update.password);
     if(update.password != ""){
       console.log('si entra, mierda');
       Domiciliario.findById(domiciliarioId, (err, dom)=>{
