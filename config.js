ea372e65e965c0ad7f90f78cd2753b54819aa955
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-02-27 14:37:03 -0500

    cambioBD

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3002,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/appDomicilios',
+  db: process.env.MONGODB || 'mongodb://localhost:27017/BDappDomicilios',
   SECRET_TOKEN: 'AjfuSed*+aq-!1473?'
 }
