0de33988b88980454958387a7a8e1bca950d7669
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-02-27 23:16:26 -0500

    unity12

--- controllers/domiciliario.js
+++ controllers/domiciliario.js
@@ -19,7 +19,7 @@ function getDomiciliario(req,res){
 function getDomiciliarios(req, res){
   Domiciliario.find({}, (err, domiciliarios)=>{
     if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
-    if(domiciliarios.length == 0)return res.status(200).send({message:'No hay domiciliarios registrados'})
+    if(domiciliarios.length == 0)return res.status(400).send({message:'No hay domiciliarios registrados'})
 
     res.status(200).send( domiciliarios )
   })
