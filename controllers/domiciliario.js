9c0602b35bbad1e35bf926fe8179f4389c2b5623
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-01 17:49:43 -0500

    unity16

--- controllers/domiciliario.js
+++ controllers/domiciliario.js
@@ -76,21 +76,26 @@ function signUp(req,res){
     if(err) return res.status(500).send({message: err})
 
     if(dom.length != 0) return res.status(500).send({message: 'EL correo ya existe en nuestra base de datos'})
-  })
-  Domiciliario.find({id: req.body.id}, (err,dom) =>{
-    if(err) return res.status(500).send({message: err})
 
-    if(dom.length != 0) return res.status(500).send({message: 'El numero de identificación ya existe en nuestra base de datos'})
-  })
+    Domiciliario.find({id: req.body.id}, (err,dom) =>{
+      if(err) return res.status(500).send({message: err})
 
-  domiciliario.save((err)=>{
-    if(err) return res.status(500).send({message: `Error registrando nuevo domiciliario: ${err}`})
+      if(dom.length != 0) return res.status(500).send({message: 'El numero de identificación ya existe en nuestra base de datos'})
 
-    res.status(201).send({
-      token: service.createToken(domiciliario),
-      domiciliario: domiciliario
+      domiciliario.save((err)=>{
+        if(err) return res.status(500).send({message: `Error registrando nuevo domiciliario: ${err}`})
+
+        res.status(201).send({
+          token: service.createToken(domiciliario),
+          domiciliario: domiciliario
+        })
+      })
+      
     })
   })
+
+
+
 
 
 
