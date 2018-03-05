541fc684df09675d09ad511fef9a47f86f7224a9
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-05 15:04:28 -0500

    map19

--- routes/index.js
+++ routes/index.js
@@ -67,8 +67,8 @@ api.put('/register/:registerId', RegisterCtrl.updateRegister)
 api.delete('/register/:registerId', RegisterCtrl.deleteRegister)
 
 //-----------------Rutas para mapas-----------------------------//
-api.get('/maps/:mapData',function(req,res){
-  res.render('index.ejs',{data: req.params.mapData})
+api.get('/maps/:mapdata',function(req,res){
+  res.render('index.ejs',{data: req.params.mapdata})
 })
 /*api.get('/private', auth, function(req,res){
   res.status(200).send({message:'Tienes acceso'})
