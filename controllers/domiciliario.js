7808cd93ae84e0b87dbf8f6d7f8f76ffaedb56e8
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-05 13:02:39 -0500

    juan13

--- controllers/domiciliario.js
+++ controllers/domiciliario.js
@@ -29,7 +29,7 @@ function updateDomiciliario(req,res){
   let domiciliarioId = req.params.domiciliarioId
 
   let update = req.body
-  if(update.position != undefined) update.position = JSON.parse(update.position)
+  //if(update.position != undefined) update.position = JSON.parse(update.position)
 
   Domiciliario.findByIdAndUpdate(domiciliarioId, update, function(err, domiciliarioUpdated){
     if(err) return res.status(500).send({message:`Error al editar el domiciliario en la base de datos ${err}`})
