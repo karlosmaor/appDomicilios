f5afd1f7ebd7cdec36b313ea597de52befea2678
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-05 19:36:10 -0500

    map40

--- controllers/map.js
+++ controllers/map.js
@@ -5,9 +5,9 @@ const Domiciliario = require('../models/domiciliario')
 
 function showDomiciliarios(req,res){
   if(req.params.mapdata.length > 3){
-    var tipoR = multiple
+    var tipoR = 'multiple'
   }else{
-    var tipoR = single
+    var tipoR = 'single'
   }
   res.render('index.ejs',{tipo: tipoR, datos: req.params.mapdata})
 }
