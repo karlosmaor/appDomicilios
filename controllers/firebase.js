2badcb8293eb05ceb42551e04b76aac0304eacef
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-07 11:50:42 -0500

    add firebase

---
+++ controllers/firebase.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var admin = require('firebase-admin')
+var AdminAccount = require('../deone-sdk.json')
+
+admin.initilizeApp({
+  credential: admin.credential.cert(AdminAccount),
+  databaseURL: 'https://deone-1519430782017.firebaseio.com/'
+})
+
+exports.module = admin
