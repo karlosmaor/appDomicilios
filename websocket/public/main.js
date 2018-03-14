b9c6fe53ab4ec7b093a18353c9f90d06e4b5b2ff
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-14 17:05:17 -0500

    WS14

--- websocket/public/main.js
+++ websocket/public/main.js
@@ -2,4 +2,14 @@ var socket = io.connect('http://146.71.79.215:8030', {'forceNew': true})
 
 socket.on('messages', function(data){
   console.log(data);
+  render(data)
 })
+
+function render(data){
+  var html = `<div>
+                <strong>${data.author}</strong>:
+                <em>${data.text}</em>
+              </div>`;
+
+  document.getElementById('messages').innerHTML = html;
+}
