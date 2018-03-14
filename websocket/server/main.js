ba789967aecfc379d7d27a75263029742ec9affd
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-03-14 14:51:47 -0500

    etet

--- websocket/server/main.js
+++ websocket/server/main.js
@@ -3,6 +3,10 @@ var app = express()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
 
+app.get('/', function(req,res){
+  res.status(200).send('Hola Mundo')
+})
+
 server.listen(8030, function (){
   console.log('Servidor Websocket corriendo...');
 })
